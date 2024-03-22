45e9538a048e14e326b778050a0c3c63542f77ef
Burcu Mengu <burcumengu@gmail.com>
2024-03-22 18:14:40 +0300

    push -u origin main

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+$("btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+
+    animatePress(userChosenColour);
+
+});
+
+
+
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
 
@@ -10,9 +25,23 @@ function nextSequence() {
     gamePattern.push(randomChosenColour);
 
 
-$("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-audio.play();
+    playSound(userChosenColour);
+
+}
+
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    },100);
 
 }
